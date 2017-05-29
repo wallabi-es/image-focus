70d3ef7a572b6c02754216339d67238060fda3da
Danny van Holten <info@dannyvanholten.com>
2017-05-29 23:24:07 +0200

    Initial Commit

---
+++ image-focus.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Image Focus: Image crop by focuspoint
+Plugin URI: https://github.com/dannyvanholten/image-focus
+Description: Image Crop plugin with which you can crop your images by focuspoint.
+Version: 0.1
+Author: Danny van Holten
+Author URI: http://www.dannyvanholten.com/
+Copyright: Danny van Holten
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+} // Exit if accessed directly
+
+// Define multiple necessary constants
+define('IMAGEFOCUS_VERSION', '0.1');
+define('IMAGEFOCUS_TEXTDOMAIN', 'image-focus');
+define('IMAGEFOCUS_LANGUAGES', dirname(plugin_basename(__FILE__)) . '/languages/');
+
+define('IMAGEFOCUS_ASSETS', plugin_dir_url(__FILE__));
+define('IMAGEFOCUS_RESOURCES', __DIR__ . '/resources/');
+
+// Use composer to autoload our classes
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Initiate the field!
+new ImageFocus();
---
+++ resources/ImageFocus.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ImageFocus;
+
+class ImageFocus
+{
+    public function __construct()
+    {
+        $this->addHooks();
+    }
+
+    /**
+     * Make sure all hooks are being executed.
+     */
+    private function addHooks()
+    {
+        add_action('admin_init', [$this, 'loadTextDomain']);
+    }
+
+    /**
+     * Load the gettext plugin textdomain located in our language directory.
+     */
+    public function loadTextDomain()
+    {
+        load_plugin_textdomain(IMAGEFOCUS_TEXTDOMAIN, false, IMAGEFOCUS_LANGUAGES);
+    }
+}
