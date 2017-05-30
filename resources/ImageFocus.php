24b80641b12015068304d89135aca25a58160051
Harwin Borger <harwin@studioborger.nl>
2017-05-30 20:11:42 +0200

    Load jquery-focuspoint

--- resources/ImageFocus.php
+++ resources/ImageFocus.php
@@ -4,24 +4,34 @@ namespace ImageFocus;
 
 class ImageFocus
 {
-    public function __construct()
-    {
-        $this->addHooks();
-    }
+	public function __construct()
+	{
+		$this->addHooks();
+		$this->enqueueAssets();
+	}
 
-    /**
-     * Make sure all hooks are being executed.
-     */
-    private function addHooks()
-    {
-        add_action('admin_init', [$this, 'loadTextDomain']);
-    }
+	/**
+	 * Make sure all hooks are being executed.
+	 */
+	private function addHooks()
+	{
+		add_action('admin_init', [$this, 'loadTextDomain']);
+	}
 
-    /**
-     * Load the gettext plugin textdomain located in our language directory.
-     */
-    public function loadTextDomain()
-    {
-        load_plugin_textdomain(IMAGEFOCUS_TEXTDOMAIN, false, IMAGEFOCUS_LANGUAGES);
-    }
+	/**
+	 * Enqueues all necessary CSS and Scripts
+	 */
+	private function enqueueAssets()
+	{
+		wp_enqueue_script('jquery-focuspoint',
+			plugins_url('bower_components/jquery-focuspoint.min.js', dirname(__FILE__)), ['jquery']);
+	}
+
+	/**
+	 * Load the gettext plugin textdomain located in our language directory.
+	 */
+	public function loadTextDomain()
+	{
+		load_plugin_textdomain(IMAGEFOCUS_TEXTDOMAIN, false, IMAGEFOCUS_LANGUAGES);
+	}
 }
