b0cd751ccc4f3191483379f60324f3cad5fdbe03
Danny van Holten <info@dannyvanholten.com>
2017-06-01 22:00:24 +0200

    FocusPoint is now a seperate class

--- resources/ImageFocus.php
+++ resources/ImageFocus.php
@@ -7,6 +7,7 @@ class ImageFocus
     public function __construct()
     {
         $this->addHooks();
+        $this->loadFocusPoint();
     }
 
     /**
@@ -15,50 +16,6 @@ class ImageFocus
     private function addHooks()
     {
         add_action('admin_init', [$this, 'loadTextDomain']);
-        add_action('admin_post_thumbnail_html', [$this, 'addFocusFeatureImageEditorLink'], 10, 2);
-        add_action('wp_ajax_initialize-crop', [$this, 'initializeCrop']);
-        add_action('admin_enqueue_scripts', [$this, 'loadScripts']);
-    }
-
-    /**
-     * Enqueues all necessary CSS and Scripts
-     */
-    public function loadScripts()
-    {
-//        wp_enqueue_script('wp-api'); // @todo activate for backbone integration
-//
-//        wp_enqueue_script('jquery-focuspoint',
-//            plugins_url('bower_components/jquery-focuspoint/js/jquery.focuspoint.min.js', dirname(__FILE__)),
-//            ['jquery']); // @todo activate for feature thumbnail crop previews
-
-        wp_enqueue_script('image-focus-js', IMAGEFOCUS_ASSETS . 'js/focuspoint.min.js', ['jquery']);
-        wp_enqueue_style('image-focus-css', IMAGEFOCUS_ASSETS . 'css/style.min.css');
-    }
-
-    public function initializeCrop()
-    {
-        // Check if we've got all the data
-        if (null === $_POST['percentageX'] || null === $_POST['percentageY']) {
-            die(
-            json_encode(
-                [
-                    'success' => false,
-                ]
-            )
-            );
-        }
-
-        $crop = new Crop();
-        $crop->cropImage(5, $_POST['percentageX'], $_POST['percentageY']);
-
-        // Return succes
-        die(
-        json_encode(
-            [
-                'success' => true,
-            ]
-        )
-        );
     }
 
     /**
@@ -67,5 +24,9 @@ class ImageFocus
     public function loadTextDomain()
     {
         load_plugin_textdomain(IMAGEFOCUS_TEXTDOMAIN, false, IMAGEFOCUS_LANGUAGES);
+    }
+
+    private function loadFocusPoint() {
+        new FocusPoint();
     }
 }
