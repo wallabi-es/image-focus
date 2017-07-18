436e1db2f3587d40c8fe2d967a75938614c5b4a9
Danny van Holten <info@dannyvanholten.com>
2017-07-18 13:29:45 +0200

    Front End buttons for the featured image

--- resources/FeaturedImage.php
+++ resources/FeaturedImage.php
@@ -20,12 +20,57 @@ class FeaturedImage
      */
     private function addHooks()
     {
-        add_action('admin_post_thumbnail_html', [$this, 'editFeaturedImage']);
+        add_action('admin_post_thumbnail_html', [$this, 'editFeaturedImage'], 1, 3);
     }
 
-    public function editFeaturedImage($content)
+    /**
+     * @param $content
+     * @param $postId
+     * @param $thumbnailId
+     * @return string
+     */
+    public function editFeaturedImage($content, $postId, $thumbnailId)
     {
-        td($content);
-        return $content .= '<p>The Featured Image is an image that is chosen as the representative image for Posts or Pages. Click the link above to add or change the image for this post. </p>';
+        // Bail early if there is no thumbnail ID as we do not need to edit the empty featured image box
+        if (!$thumbnailId || !get_post($thumbnailId)) {
+            return $content;
+        }
+
+        // Get the additional image size and check if a thumbnail is set is set
+        $imageSize = isset(wp_get_additional_image_sizes()['post-thumbnail']) ? 'post-thumbnail' : [266, 266];
+
+        /** This filter is documented in wp-admin/includes/post.php */
+        $imageSize = apply_filters('admin_post_thumbnail_size', $imageSize, $thumbnailId, get_post($postId));
+        $thumbnail = wp_get_attachment_image($thumbnailId, $imageSize);
+
+        // Bail early if there is no thumbnail as we do not need to edit the empty featured image box
+        if (empty($thumbnail)) {
+            return $content;
+        }
+
+        // Generate the featured image html
+        $featuredImage = '<div class="featured-image image-focus-actions hide-if-no-js">%s%s</div>';
+
+        // Generate it's contents
+        $featuredImageActions = '<div class="image-focus-actions__bar">
+                                    <a href="#" id="crop-post-thumbnail" class="image-focus-actions__item dashicons dashicons-image-crop"></a>
+                                    <a href="' . get_upload_iframe_src('image', $postId) . '" aria-describedby="set-post-thumbnail-desc" id="set-post-thumbnail" class="thickbox image-focus-actions__item dashicons dashicons-edit"></a>
+                                    <a href="#" id="remove-post-thumbnail" class="image-focus-actions__item dashicons dashicons-no"></a>
+                                </div>';
+
+        // Fill the html with the content
+        $content = sprintf($featuredImage,
+            $thumbnail,
+            $featuredImageActions
+        );
+
+        // Add a explanatory text
+        $content .= '<p class="hide-if-no-js howto" id="set-post-thumbnail-desc">' . __('Click the buttons in the image to crop, change or remove the image.',
+                IMAGEFOCUS_TEXTDOMAIN) . '</p>';
+
+        // Add the thumbnail id to a hidden input field
+        $content .= '<input type="hidden" id="_thumbnail_id" name="_thumbnail_id" value="' . esc_attr($thumbnailId) . '" />';
+
+        return $content;
     }
 }
