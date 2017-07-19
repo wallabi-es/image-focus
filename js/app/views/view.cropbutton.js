22a74084f7e730bd6206d53c4af60ea69e97302b
Harwin Borger <harwin@studioborger.nl>
2017-07-19 22:48:55 +0200

    Move save attachment data to the correct place

--- js/app/views/view.cropbutton.js
+++ js/app/views/view.cropbutton.js
@@ -18,7 +18,7 @@
 			this.$cropButton = $('.' + IFA.css.imageFocus._button);
 
 			//Set action to button for ajax call
-			this.$cropButton.on('click', _.bind(this.saveAttachment, this));
+			this.$cropButton.on('click', _.bind(this.attachment.saveAttachmentData, this.attachment));
 
 			this.attachment.on('change:differState', this.differStateHandler, this);
 			this.attachment.on('change:ajaxState', this.ajaxStateHandler, this);
@@ -41,39 +41,6 @@
 			this.$cropButton.toggleClass(IFA.css.button._disabled, disableState);
 			this.$cropButton.toggleClass(IFA.css.button._primary, highlightState);
 			this.$cropButton.text(text);
-		},
-
-		saveAttachment: function(){
-			var self  = this;
-			this.attachment.set('ajaxState', 'cropping');
-			this.attachment.save({}, {
-				url: ajaxurl + '?action=initialize-crop',
-				success: function (collection, data, options)
-				{
-					// you can pass additional options to the event you trigger here as well
-					// If we have data use that
-					if (data.success === true) {
-						try {
-							//Check if we received the correct object
-							self.attachment.validateFocusPoint(data);
-
-							//Store focuspoint and use 'set' for to trigger events
-							self.attachment.set({'focusPointOrigin': data.focusPoint});
-							self.attachment.set('ajaxState', 'success');
-
-						} catch (error) {
-							console.log(error);
-							self.attachment.set('ajaxState', 'failed');
-						}
-					}
-				},
-				error: function (collection, response, options)
-				{
-					// you can pass additional options to the event you trigger here as well
-					self.attachment.set('ajaxState', 'failed');
-					console.log(response);
-				}
-			});
 		},
 
 		differStateHandler: function ()
